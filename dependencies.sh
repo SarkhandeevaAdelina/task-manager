2d58bdc29436df80240f025d58562a8844362c1b
SarkhandeevaAdelina <112819129+SarkhandeevaAdelina@users.noreply.github.com>
2024-05-17 05:25:13 +0300

    Add files via upload

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+sudo apt-get install cmake \
+     build-essential \
+     libgl1-mesa-dev \
+     libglu1-mesa-dev \
+     libx11-xcb-dev \
+     libxcb-glx0-dev \
+     qtbase5-dev \
+     qtdeclarative5-dev \
+     qttools5-dev \
+     qttools5-dev-tools \
+     qtcreator
